27cf63035278ab3ccae62b9cfebecdf702e2415e
谷本　祐一 <ytanimot@MBP-15UAC-084.local>
2017-08-25 09:00:13 +0900

    recursive function sample

---
+++ recurvive_samples.hs
@@ -0,0 +1,18 @@
+-- 再帰の練習的な
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "empty list"
+maximum' [x] = x
+maximum' (x:xs) = max x (maximum' xs)
+
+-- おそらくここでのiはNumかつOrdの型クラスだと定義されている
+replicates' :: (Num i, Ord i) => i -> a -> [a]
+replicates' n x
+    | n <= 0 = []
+    | otherwise = x:replicates' (n - 1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0   = []
+take' _ []     = []
+take' n (x:xs) = x : take' (n - 1) xs
