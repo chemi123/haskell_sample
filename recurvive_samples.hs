17c70843b8c059b83e79a176166a1703269f5a1b
谷本　祐一 <ytanimot@MBP-15UAC-084.local>
2017-08-27 22:18:29 +0800

    fix

--- recurvive_samples.hs
+++ recurvive_samples.hs
@@ -6,13 +6,22 @@ maximum' [x] = x
 maximum' (x:xs) = max x (maximum' xs)
 
 -- おそらくここでのiはNumかつOrdの型クラスだと定義されている
+-- replicates' :: (Num i, Ord i) => i -> a -> [a]
+-- replicates' n x
+--     | n <= 0 = []
+--     | otherwise = x:replicates' (n - 1) x
+
 replicates' :: (Num i, Ord i) => i -> a -> [a]
 replicates' n x
     | n <= 0 = []
-    | otherwise = x:replicates' (n - 1) x
+    | otherwise = x : replicates' (n - 1) x
 
 take' :: (Num i, Ord i) => i -> [a] -> [a]
 take' n _
-    | n <= 0   = []
-take' _ []     = []
+    | n <= 0 = []
+take' _ [] = []
 take' n (x:xs) = x : take' (n - 1) xs
+
+reverse' :: (Ord a) => [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
