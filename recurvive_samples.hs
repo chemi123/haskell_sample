67c43d56cac22cb3dd350b382dcb093102c51eac
谷本　祐一 <ytanimot@MBP-15UAC-084.local>
2017-08-27 22:52:40 +0800

    add some funcs

--- recurvive_samples.hs
+++ recurvive_samples.hs
@@ -25,3 +25,17 @@ take' n (x:xs) = x : take' (n - 1) xs
 reverse' :: (Ord a) => [a] -> [a]
 reverse' [] = []
 reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x : repeat' x
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' _ [] = False
+elem' a (x:xs)
+    | a == x = True
+    | otherwise = a `elem'` xs
